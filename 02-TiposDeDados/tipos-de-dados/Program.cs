08762dd35bf7a4ade08e27e4f0ab4be15ad7f4e3
alencard <109930928+alencard@users.noreply.github.com>
2023-06-26 16:32:22 -0400

    Add files via upload

---
+++ 02-TiposDeDados/tipos-de-dados/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = true;
+            char gen = 'M';
+            char letra = '\u0041';
+            sbyte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 3.5;
+            string nome = "Maria Green";
+            object obj1 = "Alex";
+            object obj2 = 4.5f;
+
+            Console.WriteLine(t);
+            Console.WriteLine(gen);
+            Console.WriteLine(letra);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+        }
+    }
+}
---
+++ 03-propriedadesInt/propriedadesInt/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+            Console.WriteLine(sbyte.MinValue);
+            Console.WriteLine(decimal.MaxValue);
+        }
+    }
+
+}
+
