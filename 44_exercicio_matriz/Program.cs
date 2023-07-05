ade905e962f5906a718582352c4d3abf7d8b7063
Thiago Alencar <109930928+alencard@users.noreply.github.com>
2023-07-04 20:14:39 -0400

    Add files via upload

---
+++ 44_exercicio_matriz/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _44_exercicio_matriz
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] mtz = new int[n, n];
+            int[] mainDiagonal = new int[n];
+            int negNums = 0;
+
+            for (int i = 0; i < mtz.GetLength(0); i++)
+            {
+                string[] rows = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < mtz.GetLength(1); j++)
+                {
+                    int item = int.Parse(rows[j]);
+                    mtz[i, j] = item;
+
+                    if (i == j)
+                    {
+                        mainDiagonal[i] = item;
+                    }
+
+                    if (item < 0)
+                    {
+                        negNums++;
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Main diagonal:");
+
+            foreach (int item in mainDiagonal)
+            {
+                Console.Write($"{item} ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"Negative numbers = {negNums}");
+        }
+    }
+}
---
+++ 45_exercicio_matriz_2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _45_exercicio_matriz_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] rowCol = Console.ReadLine().Split(' ');
+            int rows = int.Parse(rowCol[0]);
+            int cols = int.Parse(rowCol[1]);
+            int[,] mtz = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                string[] numsRows = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < cols; j++)
+                {
+                    int item = int.Parse(numsRows[j]);
+                    mtz[i, j] = item;
+                }
+            }
+
+            int choseNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (mtz[i, j] == choseNum)
+                    {
+                        Console.WriteLine($"Position: Row {i + 1}, Column {j + 1}");
+                        if (j > 0)
+                        {
+                            Console.WriteLine("Left: " + mtz[i, j - 1]);
+                        }
+                        if (i > 0)
+                        {
+                            Console.WriteLine("Up: " + mtz[i - 1, j]);
+                        }
+                        if (j < cols - 1)
+                        {
+                            Console.WriteLine("Right: " + mtz[i, j + 1]);
+                        }
+                        if (i < rows - 1)
+                        {
+                            Console.WriteLine("Down: " + mtz[i + 1, j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
