73077866543333a83141aa4f2c2c76e6154d1258
Thiago <thithitela@gmail.com>
2023-07-08 16:34:01 -0400

    Try Catch

---
+++ 64TryCatch/64TryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _64TryCatch
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n1;
+            int n2;
+            int result;
+
+            try
+            {
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+
+                result = n1 / n2;
+
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine($"Division by zero is not allowed!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Only numbers are allowed!");
+            }
+        }
+    }
+}
