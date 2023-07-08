6ade34f7898c4643c51871ad8d3fea0ef2f0cd42
Thiago <thithitela@gmail.com>
2023-07-08 08:58:18 -0400

    Polimorfismo

---
+++ 58_metodos selados/58_metodos selados/Entities/SavingsAccount.cs
@@ -0,0 +1,38 @@
+﻿namespace _58_metodos_selados.Entities
+{
+    /*sealed*/class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        /*
+        //sobrescrevendo mudando o metodo
+        public override void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+        */
+
+        public sealed override void Withdraw(double amount)
+        {
+            //aproveito o metodo original
+            base.Withdraw(amount);
+            // e faço as alterações que preciso
+            Balance -= 2;
+        }
+    }
+}
---
+++ 58_metodos selados/58_metodos selados/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,15 @@
+﻿
+namespace _58_metodos_selados.Entities
+{
+    // classes seladas nao podem ser pai de ninguem
+    class SavingsAccountPlus : SavingsAccount
+    {
+        /*
+        // nao posso sobrescrever um metodo selado
+        public override void Withdraw(double amount)
+        {
+
+        }
+        */
+    }
+}
