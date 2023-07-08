afcee79fa42f032b23ef052af41a80f40f060d08
Thiago <thithitela@gmail.com>
2023-07-08 19:00:02 -0400

    Exercicio de Fixacao - Try, Catch

---
+++ 66ExercicioDeFixacao/66ExercicioDeFixacao/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using _66ExercicioDeFixacao.Entities.Exceptions;
+
+namespace _66ExercicioDeFixacao.Entities
+{
+    internal class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; private set; }
+        public double WithdrawLimit { get; private set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (WithdrawLimit < amount)
+            {
+                throw new DomainException("The amount is bigger than the withdraw limit.");
+            }
+
+            if (Balance < amount)
+            {
+                throw new DomainException("The amount is bigger than the account balance.");
+            }
+
+            Balance -= amount;
+        }
+    }
+}
---
+++ 66ExercicioDeFixacao/66ExercicioDeFixacao/Program.cs
@@ -0,0 +1,54 @@
+﻿using _66ExercicioDeFixacao.Entities;
+using _66ExercicioDeFixacao.Entities.Exceptions;
+using System.Globalization;
+
+namespace _66ExercicioDeFixacao
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Account acc;
+                int number;
+                string holder;
+                double initialBalance;
+                double withdrawLimit;
+                double withdraw;
+
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc = new(number, holder, initialBalance, withdrawLimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Withdraw(withdraw);
+
+                Console.WriteLine($"New Balance: {acc.Balance.ToString("f2", CultureInfo.InvariantCulture)}");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
