9759e6194609ffab31cf949857b9d166dbecfc06
Thiago <thithitela@gmail.com>
2023-07-07 21:35:03 -0400

    Herança, Upcasting e Down casting

---
+++ 57Heranca/57Heranca/Entities/BusinessAccount.cs
@@ -0,0 +1,26 @@
+﻿namespace _57Heranca.Entities
+{
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+
+        }
+
+        // como aproveitar o construtor da classe pai
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
