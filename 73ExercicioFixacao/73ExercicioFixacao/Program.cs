ce2cc34bfbf7d07a08586fcb5cdb20a2fc1de0d1
Thiago <thithitela@gmail.com>
2023-07-09 14:21:55 -0400

    Exercicio de Fixação

---
+++ 73ExercicioFixacao/73ExercicioFixacao/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _73ExercicioFixacao
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sourcePath;
+            string targetPath;
+            string[] lines;
+
+            try
+            {
+                sourcePath = @"C:\Users\thith\OneDrive\programacao\back-end\csharp\curso-csharp\73ExercicioFixacao\73ExercicioFixacao\Files\file.txt";
+                targetPath = @"C:\Users\thith\OneDrive\programacao\back-end\csharp\curso-csharp\73ExercicioFixacao\73ExercicioFixacao\Files\summary.csv";
+
+                lines = File.ReadAllLines(sourcePath);
+
+                using StreamWriter sw = File.AppendText(targetPath);
+
+                foreach(string line in lines)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
