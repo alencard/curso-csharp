dedb7599b128e5bb82371fe5ac46c775910f39c4
Thiago <thithitela@gmail.com>
2023-07-09 11:31:11 -0400

    Directory

---
+++ 71Directory/71Directory/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _71Directory
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"..\..\..\MyFolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Folders: ");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("Files: ");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                Directory.CreateDirectory(@"..\..\..\MyFolder\new");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
