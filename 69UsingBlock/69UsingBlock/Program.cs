22fce8b608a524b7efdb11f10ea84293ed67bbca
Thiago <thithitela@gmail.com>
2023-07-08 20:34:39 -0400

    Using Files

---
+++ 69UsingBlock/69UsingBlock/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _69UsingBlock
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+
+                string path = @"..\..\..\Files\file.txt";
+
+                using StreamReader sr = File.OpenText(path);
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
