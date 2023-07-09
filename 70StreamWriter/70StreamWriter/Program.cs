3b4cc9c39cd5004f441872852e33c6fc65e716f1
Thiago <thithitela@gmail.com>
2023-07-08 20:41:55 -0400

    StreamWriter

---
+++ 70StreamWriter/70StreamWriter/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _70SreamWriter
+{
+    class Program
+    {
+        static void Main()
+        {
+            string sourcePath = @"..\..\..\Files\file.txt";
+            string targetPath = @"..\..\..\Files\file1.txt";
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                //append adiciona conteudo no fim do arquivo
+                using StreamWriter sw = File.AppendText(targetPath);
+
+                foreach (string line in lines)
+                {
+                    sw.WriteLine(line.ToUpper());
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+        }
+    }
+}
