ba350d7e9202939d64bf4dc4b52ea4174b0eaee2
Thiago <thithitela@gmail.com>
2023-07-08 20:20:37 -0400

    FileStream e StreamReader

---
+++ 68FileStream/68FileStream/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _68FileStream
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"..\..\..\Files\file.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+            finally
+            {
+                sr?.Close();
+            }
+        }
+    }
+}
