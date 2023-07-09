8043495c1148b8268e1d89538104e5a5afdcc248
Thiago <thithitela@gmail.com>
2023-07-09 13:28:07 -0400

    Path

---
+++ 72Path/72Path/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _72Path
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"..\..\..\MyFolder\file.txt";
+
+            Console.WriteLine(Path.DirectorySeparatorChar);
+            Console.WriteLine(Path.PathSeparator);
+            Console.WriteLine(Path.GetDirectoryName(path));
+            Console.WriteLine(Path.GetFileName(path));
+            Console.WriteLine(Path.GetExtension(path));
+            Console.WriteLine(Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine(Path.GetFullPath(path));
+            Console.WriteLine(Path.GetTempPath());
+        }
+    }
+}
