d77fa5c6dd47ed90254e7f61b818fddeba82456f
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-18 13:19:44 -0400

    Parcialmente finalizado

---
+++ 78Exercicio/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace _78Exercicio.Entities
+{
+    internal class Product
+    {
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ 78Exercicio/Program.cs
@@ -0,0 +1,15 @@
+﻿using _78Exercicio.Services;
+using _78Exercicio.Entities;
+
+List<int> list = new();
+
+Console.WriteLine("Enter N: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int x = int.Parse(Console.ReadLine());
+    list.Add(x);
+}
+
+CalculationService<Product> calculation = new();
