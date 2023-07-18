2662bd2ba15adf6d23d15ea3c105f50e8bca1003
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-18 11:23:58 -0400

    Solução com uso de tipos genéricos

---
+++ 77Generics1/Entities/PrintService.cs
@@ -0,0 +1,44 @@
+﻿namespace _77Generics1.Entities
+{
+    internal class PrintService<T>
+    {
+        private readonly T[] _values = new T[10];
+        private int _count = 0;
+
+        public void AddValues(T value)
+        {
+            if (_count >= 10)
+            {
+                throw new InvalidOperationException("PrintService is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public T First()
+        {
+            if (_values.Length == 0)
+            {
+                throw new InvalidOperationException("PrintService is Empty");
+            }
+
+            return _values[0];
+        }
+
+        public void Print()
+        {
+            int i = 0;
+
+            Console.Write("[");
+
+            while (i < _values.Length - 1)
+            {
+                Console.Write($"{_values[i]}, ");
+                i++;
+            }
+            Console.Write(_values[^1]);
+
+            Console.WriteLine("]");
+        }
+    }
+}
---
+++ 77Generics1/Program.cs
@@ -0,0 +1,20 @@
+﻿using _77Generics1.Entities;
+
+
+try
+{
+    var printService = new PrintService<string>();
+
+    Console.Write("How many numbers? ");
+    int qttNums = int.Parse(Console.ReadLine());
+
+    for(int i = 1; i <= qttNums; i++)
+    {
+        string x = Console.ReadLine();
+        printService.AddValues(x);
+    }
+}
+catch (IndexOutOfRangeException e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
