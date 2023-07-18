3c0d6064610db783a5eb3404f7bdf77322df7ff7
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-18 08:16:15 -0400

    Novo IComparable

---
+++ 76IComparable/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+
+namespace _76IComparable.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] data = csvEmployee.Split(',');
+            Name = data[0];
+            Salary = double.Parse(data[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Salary: R${Salary.ToString("f2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj is not Employee)
+            {
+                throw new ArgumentException("");
+            }
+
+            Employee other = obj as Employee;
+
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ 76IComparable/Program.cs
@@ -0,0 +1,34 @@
+﻿using _76IComparable.Entities;
+
+namespace _76IComparable
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Employee> list = new();
+            string path = @"C:\Users\05357762252\Documents\cSharp\curso\76IComparable\docsParaTeste\employees.csv";
+
+            try
+            {
+                using StreamReader sr = new(path);
+
+                while (!sr.EndOfStream)
+                {
+                    list.Add(new Employee(sr.ReadLine()));
+                }
+
+                list.Sort();
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+            foreach (Employee s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
