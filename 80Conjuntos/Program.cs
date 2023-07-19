1139d252a12f336912d4098ce85e3b2bba84f848
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-19 11:17:41 -0400

    Métodos do SortedSet

--- 80Conjuntos/Program.cs
+++ 80Conjuntos/Program.cs
@@ -1,4 +1,40 @@
-﻿HashSet<string> set = new()
+﻿namespace _80Conjuntos
+{
+    class Program
+    {
+        static void Main()
+        {
+            SortedSet<int> set = new() { 10, 2, 4, 5, 6, 8, 0 };
+            SortedSet<int> set2 = new() { 5, 6, 7, 8, 9, 10 };
+
+            //union
+            SortedSet<int> union = new(set);
+            union.UnionWith(set2);
+            PrintCollection(union);
+
+            //intersection
+            SortedSet<int> intersection = new(set);
+            intersection.IntersectWith(set2);
+            PrintCollection(intersection);
+
+            //difference - exclui elementos da lista que é passada como parametro
+            SortedSet<int> difference = new(set);
+            difference.ExceptWith(set2);
+            PrintCollection(difference);
+        }
+
+        static void PrintCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T item in collection)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+/*
+HashSet<string> set = new()
 {
     "TV",
     "Notebook",
@@ -13,3 +49,5 @@ foreach (var item in set)
 {
     Console.WriteLine(item);
 }
+*/
+
