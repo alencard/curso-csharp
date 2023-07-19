6517da44a2df23c910ed9d1425560be21eb5a8d2
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-19 12:53:47 -0400

    Exercicio Coleções - 1

---
+++ 81ExercicioResolvido/Entities/LogRecord.cs
@@ -0,0 +1,36 @@
+﻿namespace _81ExercicioResolvido.Entities
+{
+    internal class LogRecord
+    {
+        public string UserName { get; private set; }
+        public DateTime MomentOfAccess { get; private set; }
+
+        public LogRecord(string userName, DateTime momentOfAccess)
+        {
+            UserName = userName;
+            MomentOfAccess = momentOfAccess;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not LogRecord)
+            {
+                throw new ArgumentException("The data type is incorrect");
+            }
+
+            LogRecord other = obj as LogRecord;
+
+            return UserName.Equals(other.UserName);
+        }
+
+        public override string ToString()
+        {
+            return $"{UserName}, {MomentOfAccess}";
+        }
+    }
+}
---
+++ 81ExercicioResolvido/Program.cs
@@ -0,0 +1,26 @@
+﻿using _81ExercicioResolvido.Entities;
+using System.Globalization;
+
+
+HashSet<LogRecord> logRecords = new();
+int qttUsers = 0;
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+
+string[] lines = File.ReadAllLines(path);
+
+foreach (string line in lines)
+{
+    string[] data = line.Split(' ');
+    string userName = data[0];
+    DateTime momentOfAccess = DateTime.ParseExact(data[1], "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture);
+    logRecords.Add(new LogRecord(userName, momentOfAccess));
+}
+
+foreach (LogRecord logRecord in logRecords)
+{
+    qttUsers++;
+}
+
+Console.WriteLine($"Total users: {qttUsers}");
