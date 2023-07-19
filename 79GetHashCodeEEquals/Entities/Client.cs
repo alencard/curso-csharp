54b96f4ecabc844fefb0f60a79533168f3eb3191
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-19 09:59:14 -0400

    Get Hash Code e Equals

---
+++ 79GetHashCodeEEquals/Entities/Client.cs
@@ -0,0 +1,31 @@
+﻿namespace _79GetHashCodeEEquals.Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Client(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is not Client)
+            {
+                throw new ArgumentException("The type is wrong");
+            }
+
+            Client other = obj as Client;
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ 79GetHashCodeEEquals/Program.cs
@@ -0,0 +1,31 @@
+﻿using _79GetHashCodeEEquals.Entities;
+
+/*
+string a = "Maria";
+string b = "Lucas";
+
+for (int i = 0; i < 5; i++)
+{
+    int num1 = a.GetHashCode();
+    int num2 = b.GetHashCode();
+    int num3 = a.GetHashCode();
+
+    Console.WriteLine(num1);
+    Console.WriteLine(num2);
+    Console.WriteLine(num3);
+    Console.WriteLine();
+}
+
+while (num1 != num2)
+{
+    Console.WriteLine(num1 == num2);
+}
+*/
+
+Client a = new("Maria", "maria@email.com");
+Client b = new("Alex", "alex@email.com");
+
+Console.WriteLine(a.Equals(b));
+Console.WriteLine(a == b);
+Console.WriteLine(a.GetHashCode());
+Console.WriteLine(b.GetHashCode());
