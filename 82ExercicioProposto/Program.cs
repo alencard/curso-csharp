ca5683d8980698837e0630b930467e5ec97099fd
Thiago <thithitela@gmail.com>
2023-07-19 17:11:55 -0400

    término da tarefa

--- 82ExercicioProposto/Program.cs
+++ 82ExercicioProposto/Program.cs
@@ -1,16 +1,30 @@
-﻿using _82ExercicioProposto.Entities;
-
-
-HashSet<Course> courses = new();
+﻿HashSet<int> courseA = new();
+HashSet<int> courseB = new();
+HashSet<int> courseC = new();
 
 Console.Write("How many students for course A? ");
-int aStudents = int.Parse(Console.ReadLine());
-a.AddStudents(aStudents);
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    courseA.Add(int.Parse(Console.ReadLine()));
+}
 
 Console.Write("How many students for course B? ");
-int bStudents = int.Parse(Console.ReadLine());
-b.AddStudents(bStudents);
+n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    courseB.Add(int.Parse(Console.ReadLine()));
+}
 
 Console.Write("How many students for course C? ");
-int cStudents = int.Parse(Console.ReadLine());
-c.AddStudents(cStudents);
+n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    courseC.Add(int.Parse(Console.ReadLine()));
+}
+
+HashSet<int> all = new(courseA);
+all.UnionWith(courseB);
+all.UnionWith(courseC);
+
+Console.WriteLine($"Total students: {all.Count}");
