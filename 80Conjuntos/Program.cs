7aab0a2b61e2634a9e14290e5edfbdcc63e3b016
05357762252 <thiagosantos@setic.ro.gov.br>
2023-07-19 10:24:00 -0400

    HashSet

---
+++ 80Conjuntos/Program.cs
@@ -0,0 +1,15 @@
+﻿HashSet<string> set = new()
+{
+    "TV",
+    "Notebook",
+    "Tablet",
+    "Notebook",
+    "Computer"
+};
+
+//Console.WriteLine(set.Contains("computer"));
+
+foreach (var item in set)
+{
+    Console.WriteLine(item);
+}
